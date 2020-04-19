2dadc5390d7f31fef8e65c8178dd142e676fe6dd
吴启猛 <1160302203@qq.com>
2020-04-19 13:23:12 +0800

    add algorithm tree

--- src/main/java/exercise/leetcode/HowManyWater.java
+++ src/main/java/exercise/leetcode/HowManyWater.java
@@ -1,5 +1,9 @@
 package exercise.leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 42. 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
  * <p>
@@ -8,5 +12,6 @@ package exercise.leetcode;
  * 输出: 6
  */
 public class HowManyWater {
+
 
 }
---
+++ src/main/java/exercise/leetcode/TreeLevel.java
@@ -0,0 +1,60 @@
+package exercise.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 二叉树 层序遍历
+ */
+public class TreeLevel {
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                level.add(head.val);
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+
+            }
+            result.add(level);
+        }
+
+        return result;
+
+    }
+
+}
+
+
