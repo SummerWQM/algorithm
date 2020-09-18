5d35a8c83b5c1f78a5dc1e68e797c72e8c445b24
jackqmwu <jackqmwu@tencent.com>
2020-09-18 10:00:18 +0800

    add exchange link node

---
+++ src/main/java/exercise/leetcode/ExchangeLinkNode.java
@@ -0,0 +1,31 @@
+package exercise.leetcode;
+
+class ExchangeLinkNode {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+
+        // If the list has no node or has only one node left.
+        if ((head == null) || (head.next == null)) {
+            return head;
+        }
+        // Nodes to be swapped
+        ListNode secondNode = head.next;
+
+        // Swapping
+        head.next = swapPairs(secondNode.next);
+        secondNode.next = head;
+
+        // Now the head is the second node
+        return secondNode;
+    }
+
+}
+
