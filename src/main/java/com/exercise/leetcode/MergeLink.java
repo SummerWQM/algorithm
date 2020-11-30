418866576ecd99d3e65a61a40c5a6f430833d59b
jackqmwu <jackqmwu@tencent.com>
2020-11-30 14:05:00 +0800

    modify node

---
+++ src/main/java/com/exercise/leetcode/MergeLink.java
@@ -0,0 +1,51 @@
+package com.exercise.leetcode;
+
+public class MergeLink {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 每个节点逐一比较
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode tail = new ListNode(0);
+        ListNode cur = tail;
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        return tail.next;
+    }
+
+}
