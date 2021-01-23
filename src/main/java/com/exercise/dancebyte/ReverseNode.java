0a17abf7bb58afdcfbc6f148f889aee420fe7355
jackqmwu <jackqmwu@tencent.com>
2021-01-23 21:54:18 +0800

    modify a in b

--- src/main/java/com/exercise/dancebyte/ReverseNode.java
+++ src/main/java/com/exercise/dancebyte/ReverseNode.java
@@ -4,34 +4,8 @@ import com.helper.ListNode;
 
 // K 个一组 翻转链表
 public class ReverseNode {
-    //    public ListNode reverseKGroup(ListNode head, int k) {
-//
-//        Deque<ListNode> stack = new ArrayDeque<ListNode>();
-//        ListNode dummy = new ListNode(0);
-//        ListNode p = dummy;
-//
-//        while (true) {
-//            int count = 0;
-//            ListNode tmp = head;
-//            while (tmp != null && count < k) {
-//                stack.add(tmp);
-//                tmp = tmp.next;
-//                count++;
-//            }
-//            if (count != k) {
-//                p.next = head;
-//                break;
-//            }
-//            while (!stack.isEmpty()) {
-//                p.next = stack.pollLast();
-//                p = p.next;
-//            }
-//            p.next = tmp;
-//            head = tmp;
-//        }
-//        return dummy.next;
-//    }
     public ListNode reverseKGroup(ListNode head, int k) {
+
         ListNode dummy = new ListNode(0);
         dummy.next = head;
 
@@ -44,11 +18,12 @@ public class ReverseNode {
             ListNode start = pre.next;
             ListNode next = end.next;
             end.next = null;
+            // 这里翻转 1-3 start 开始的 1 -> 4
+            // 3-2-1->4->5->6
             pre.next = reverse(start);
-            start.next = next;
-            pre = start;
-
-            end = pre;
+            start.next = next;// 指向下一个翻转的开始节点  1->4
+            pre = start; // 1 节点付给 pre 方便 下一次 去 pre.next
+            end = pre; // 1 给 end 方便下一次 end 的跳转到 next
         }
         return dummy.next;
     }
@@ -62,6 +37,7 @@ public class ReverseNode {
             pre = curr;
             curr = next;
         }
+        // 返回翻转之后的链表
         return pre;
     }
 
@@ -72,10 +48,13 @@ public class ReverseNode {
         ListNode newList = null, node;
 
         while (head != null) {
+            //取出当前节点
             node = head;
+            // 移动下一个head节点
             head = head.next;
-
+            // 取出当前节点，指向 翻转的节点
             node.next = newList;
+            // 移动newlist 指针 指向最新的 翻转节点
             newList = node;
         }
         return newList;
