bb9e7230911a00d89db7654fb0f9d17827e43928
jackqmwu <jackqmwu@tencent.com>
2021-02-24 14:20:14 +0800

    modify cal

---
+++ src/main/java/com/exercise/leetcode/Solution2.java
@@ -0,0 +1,33 @@
+package com.exercise.leetcode;
+
+import com.helper.ListNode;
+
+import java.util.HashSet;
+
+
+public class Solution2 {
+
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode pre = new ListNode(0);
+
+        ListNode cur = pre;
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+
+        while (head != null) {
+
+            ListNode next = head.next;
+
+            if (hashSet.add(head.val)) {
+                cur.next = head;
+                head.next = null;
+                cur = head;
+            }
+            head = next;
+        }
+
+        return pre.next;
+    }
+}
