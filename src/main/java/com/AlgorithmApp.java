2f31c246cc83fa3c94f6020ca0e80cce7878fd7b
jackqmwu <jackqmwu@tencent.com>
2021-03-24 10:52:38 +0800

    Modify algorithmapp

--- src/main/java/com/AlgorithmApp.java
+++ src/main/java/com/AlgorithmApp.java
@@ -5,6 +5,12 @@ package com;
 public class AlgorithmApp {
 
     public static void main(String[] args) {
+        double sum = 9.03;
+        for (int i = 0; i < 5; i++) {
+            sum += sum * 0.1;
+            System.out.println(sum);
+
+        }
 
     }
 }
