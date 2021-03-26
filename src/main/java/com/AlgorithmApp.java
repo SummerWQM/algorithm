cb03ca54ea9f6ba9fa09c92693fa509fad87c7b3
jackqmwu <jackqmwu@tencent.com>
2021-03-26 17:06:36 +0800

    modify common

--- src/main/java/com/AlgorithmApp.java
+++ src/main/java/com/AlgorithmApp.java
@@ -1,14 +1,13 @@
 package com;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlgorithmApp {
 
     public static void main(String[] args) {
-
-        // SpringApplication.run(AlgorithmApp.class, args);
-
+        SpringApplication.run(AlgorithmApp.class, args);
     }
 }
