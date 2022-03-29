0eaef5c151b5520d10c159570e10cbec5472d977
qimeng.wu <qimeng.wu@weimob.com>
2022-03-29 11:41:48 +0800

    modify new

--- src/main/java/com/exercise/leetcode/SalePrice.java
+++ src/main/java/com/exercise/leetcode/SalePrice.java
@@ -30,4 +30,25 @@ public class SalePrice {
         System.out.println(SalePrice.maxProfit(prices));
     }
 
+    /*
+     *https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
+     * 可以交易多次的麦盖股票
+     *
+     * 贪心算法
+     */
+
+    public int max(int[] prices) {
+        int ans = 0;
+        int n = prices.length;
+        for (int i = 1; i < n; ++i) {
+
+            int dif = prices[i] - prices[i - 1];
+            if (dif > 0) {
+                ans += dif;
+            }
+        }
+        return ans;
+
+    }
+
 }
