82ae2909dcef9b711b2088c09b290a1d9ba4ed42
qimeng.wu <qimeng.wu@weimob.com>
2022-07-17 14:56:11 +0800

    add sort version

--- src/main/java/com/exercise/leetcode/VersionSort.java
+++ src/main/java/com/exercise/leetcode/VersionSort.java
@@ -5,8 +5,7 @@ public class VersionSort {
 
     public static void main(String[] avg) {
 
-        String[] versions = new String[]{"2","1.9.0.1", "5", "3.0", "3.1", "1.7.1", "4.1.1", "2.1.3", "2.1.3", "2.0", "2.0.1", "2.1.3.0"};
-
+        String[] versions = new String[]{"2","1.9.0.1", "5", "3.11.1", "3.1", "1.7.1", "4.1.1", "2.1.3", "2.1.3", "2.0", "2.0.1", "2.1.3.0"};
 
         sortVersion(versions);
 
@@ -23,7 +22,6 @@ public class VersionSort {
         if (versions == null || versions.length == 0) {
             return;
         }
-
         for (int i = 0; i < versions.length; i++) {
             for (int j = i + 1; j < versions.length; j++) {
                 String temp = versions[j];
@@ -37,32 +35,18 @@ public class VersionSort {
     }
 
     public static int compare(String o1, String o2) {
-
-        if (o1 == null && o2 == null) {
-            return 0;
-        }
-        if (o1 == null) {
-            return -1;
-        }
-        if (o2 == null) {
-            return 1;
-        }
-
         int min = Math.min(o1.length(), o2.length()) - 1;
-        int dif = 0;
         int p = 0;
         while (p <= min + 1) {
             if (p > min) {
                 return o1.length() > p ? 1 : -1;
             }
-            dif = o1.charAt(p) - o2.charAt(p);
-            if (dif == 0) {
-                p++;
-                continue;
+            if (o1.charAt(p) != o2.charAt(p)) {
+                return o1.charAt(p) - o2.charAt(p);
             }
-            return dif;
-        }
+            p++;
 
-        return dif;
+        }
+        return 0;
     }
 }
