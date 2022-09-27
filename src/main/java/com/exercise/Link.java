387134389a98cc0dfb9710ba9eff20f70ca80942
qimeng.wu <qimeng.wu@weimob.com>
2022-09-27 14:50:42 +0800

    add ling method

--- src/main/java/com/exercise/Link.java
+++ src/main/java/com/exercise/Link.java
@@ -1,4 +1,96 @@
- static class Node {
+    public static void main(String[] avg) {
+        Node l1 = buildLink(5);
+        System.out.println(printLink(l1));
+        //  Node l2 = buildLink(6);
+        System.out.println(printLink(reversK(l1, 2)));
+//        System.out.println(printLink(merge(l1, l2)));
+        //   System.out.println(printLink(add(l1, l2)));
+    }
+
+    public static Node reversK(Node head, int k) {
+        Node hair = new Node(0);
+        hair.next = head;
+        Node pre = hair;
+        while (head != null) {
+            Node tail = pre;
+            for (int i = 0; i < k && tail != null; i++) {
+                tail = tail.next;
+            }
+            if (tail == null) {
+                break;
+            }
+            Node next = tail.next;
+            Node[] rev = doRevers(head, tail);
+            pre.next = rev[0];
+            pre = rev[1];
+
+
+            head = next;
+        }
+        return hair.next;
+    }
+
+    public static Node[] doRevers(Node head, Node tail) {
+        Node pre = tail.next;
+        Node cur = head;
+        while (pre != tail) {
+            Node next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return new Node[]{tail, head};
+    }
+
+    public static Node merge(Node l1, Node l2) {
+        Node dumy = new Node(-1);
+        Node pre = dumy;
+        while (l1 != null || l2 != null) {
+            if (l1 == null) {
+                pre.next = l2;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                pre.next = l1;
+                l1 = l1.next;
+            } else if (l1.val > l2.val) {
+                pre.next = l1;
+                l1 = l1.next;
+            } else {
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+        return dumy.next;
+    }
+
+    public static Node add(Node l1, Node l2) {
+        int carry = 0, sum = 0;
+        Node hair = new Node(-1);
+        Node pre = hair;
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            sum = x + y + carry;
+            Node node = new Node(sum % 10);
+            carry = sum / 10;
+            pre.next = node;
+            pre = node;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            pre.next = new Node(carry);
+        }
+
+        return hair.next;
+    }
+
+    static class Node {
         Node next;
         int val;
 
@@ -29,61 +121,4 @@
             cur = cur.next;
         }
         return head;
-    }
-
-    public static Node revers(Node head, int k) {
-        Node hair = new Node(0);
-        hair.next = head;
-        Node pre = hair;
-        while (head != null) {
-            Node tail = pre;
-            for (int i = 0; i < k; i++) {
-                if (tail == null) {
-                    return hair.next;
-                }
-                tail = tail.next;
-            }
-            Node next = tail.next;
-            Node[] reverse = rever(head, tail);
-            pre.next = reverse[0];
-            pre = reverse[1];
-            head = next;
-        }
-
-        return hair.next;
-    }
-
-    public static Node[] rever(Node head, Node tail) {
-        Node pre = tail.next;
-        Node cur = head;
-        while (pre != tail) {
-            Node next = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = next;
-        }
-        return new Node[]{tail, head};
-    }
-
-    public static Node numberK(Node head, int k) {
-        Node fast = head;
-        Node slow = head;
-        for (int i = 0; i < k; i++) {
-            if (fast == null) {
-                return null;
-            }
-            fast = fast.next;
-        }
-        while (fast != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-        return slow;
-    }
-
-    public static void main(String[] avg) {
-        Node link = buildLink(10);
-        System.out.println(printLink(link));
-        System.out.println(printLink(revers(link, 2)));
-        System.out.println(printLink(numberK(link, 2)));
     }
