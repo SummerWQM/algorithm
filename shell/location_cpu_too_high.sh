288f4f61a2e55209ab7d247794c1bd1101321127
qimeng.wu <qimeng.wu@weimob.com>
2023-06-03 18:42:09 +0800

    Modify t

--- shell/location_cpu_too_high.sh
+++ shell/location_cpu_too_high.sh
@@ -2,10 +2,11 @@
 top 找到java 进程 cpu 最好的
 
 # 通过ps 定位 最好的线程
-ps -mp 16268 -o THREAD,tid,time
+ps -mp 1 -o THREAD,tid,time
 
 # 计算16进制线程id
 printf "%x\n" tid
 
+13101
 # 通过堆栈工具， 导出 进程 对应的线程 堆栈信息， 取30行数据
 jstack 1 |  grep 3330 -A 30
