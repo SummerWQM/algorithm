e14952318f86074b555edee8bc2ea0aaa86a5f86
qimeng.wu <qimeng.wu@weimob.com>
2023-08-17 11:44:05 +0800

    modify tree

--- src/main/java/com/myself/exercise/TreeSolution.java
+++ src/main/java/com/myself/exercise/TreeSolution.java
@@ -159,6 +159,20 @@ public class TreeSolution {
         return Math.max(height(root.left), height(root.right)) + 1;
     }
 
+    /**
+     * 101 对称
+     * 左1=右2
+     * 右1=左2
+     *
+     * @param
+     */
+    public static boolean isSymmetric(TreeNode tree1, TreeNode tree2) {
+        if (tree1 == null && tree2 == null) return true;
+        if (tree1 == null || tree2 == null) return false;
+
+        return (tree1.val == tree2.val) && isSymmetric(tree1.left, tree2.right)
+                && isSymmetric(tree1.right, tree2.left);
+    }
 
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6};
