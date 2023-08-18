2f974f18a507c20caf128e46912cde659b03414b
qimeng.wu <qimeng.wu@weimob.com>
2023-08-18 10:43:23 +0800

    modify tree solution

--- src/main/java/com/myself/exercise/TreeSolution.java
+++ src/main/java/com/myself/exercise/TreeSolution.java
@@ -196,9 +196,29 @@ public class TreeSolution {
     }
 
     /**
+     * 543 二叉树直径
      *
      * @param args
      */
+    static int ans;
+
+    public static int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        depth(root);
+        return ans - 1;
+    }
+
+    public static int depth(TreeNode node) {
+        if (node == null) {
+            return 0; // 访问到空节点了，返回0
+        }
+        int L = depth(node.left); // 左儿子为根的子树的深度
+        int R = depth(node.right); // 右儿子为根的子树的深度
+        // 更新数的最大值  + 1 是当前节点
+        ans = Math.max(ans, L + R + 1); // 计算d_node即L+R+1 并更新ans
+        return Math.max(L, R) + 1; // 返回该节点为根的子树的深度
+    }
+
 
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3};
