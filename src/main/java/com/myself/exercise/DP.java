3cd631793775f9368bb2730a85f110fdd99e9d85
qimeng.wu <qimeng.wu@weimob.com>
2023-08-18 16:34:46 +0800

    modify return msg

--- src/main/java/com/myself/exercise/DP.java
+++ src/main/java/com/myself/exercise/DP.java
@@ -140,6 +140,27 @@ public class DP {
         return dp[n1][n2];
     }
 
+    // 64. 最小路径和
+    public int minRoad(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int row = grid.length, column = grid[0].length;
+        int[][] dp = new int[row][column];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < column; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < column; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[row - 1][column - 1];
+    }
 
     public static void main(String[] args) {
 
